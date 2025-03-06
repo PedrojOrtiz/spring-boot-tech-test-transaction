4141b940c8c469e55e83d8326f31cf36df3dd444
pedroortizsolis <portiz@territoriosinteligentes.net>
2025-03-06 00:44:56 -0500

    First commit

---
+++ src/main/java/ec/sofka/springboottechtesttransaction/SpringBootTechTestTransactionApplication.java
@@ -0,0 +1,13 @@
+package ec.sofka.springboottechtesttransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTechTestTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTechTestTransactionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ec/sofka/springboottechtesttransaction/SpringBootTechTestTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.sofka.springboottechtesttransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTechTestTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
